f0d7bde69a29953876174216f008ea528f408c3b
Houssam-Ha <hamiyahoussam@gmail.com>
2025-06-19 12:49:09 +0100

    add new file

--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
-#from django.http import HttpResponse
+# pages/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib import messages
 
-# Create your views here.
 def index(request):
     x = {
         "title": "home",
@@ -16,4 +17,24 @@ def about(request):
     return render(request, "pages/about.html")
 
 def login(request):
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     remember_me = request.POST.get('remember')
+        
+    #     # Authenticate user
+    #     user = authenticate(request, username=username, password=password)
+        
+    #     if user is not None:
+    #         auth_login(request, user)
+            
+    #         # Handle remember me functionality
+    #         if not remember_me:
+    #             request.session.set_expiry(0)  # Session expires when browser closes
+            
+    #         messages.success(request, f'Welcome back, {user.username}!')
+    #         return redirect('index')  # Redirect to home page after successful login
+    #     else:
+    #         messages.error(request, 'Invalid username or password. Please try again.')
+    
     return render(request, "pages/login.html")
