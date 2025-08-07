294bbb51850e09ba9dce55a45651721a3f5ebf45
Houssam-Ha <hamiyahoussam@gmail.com>
2025-08-07 11:46:59 +0100

    add new file

--- clubs/admin.py
+++ clubs/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Club
-# Register your models here.
-admin.site.register(Club)
+
+class ClubAdmin(admin.ModelAdmin):
+    list_display = ('nom_club', 'date_creation', 'active')
+    filter_horizontal = ('members',)  
+
+admin.site.register(Club, ClubAdmin)
